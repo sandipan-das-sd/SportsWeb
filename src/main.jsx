f08e4de4cbcdad582faf117585632850c41b95cf
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-07-03 18:00:24 +0530

    admi fixed

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import App from "./App.jsx";
 import "./index.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { SidebarProvider } from './AdminPanel/context/sidebarContext.jsx';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <SidebarProvider>
+      <App />
+    </SidebarProvider>
   </React.StrictMode>
 );
