14c838880218a690824df4f6d71a45248e80ddbe
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-07-04 15:10:32 +0530

    updated

--- client/src/AdminPanel/AdminApp.jsx
+++ client/src/AdminPanel/AdminApp.jsx
@@ -1,14 +1,16 @@
 import './App.css';
 import Sidebar from './layout/Sidebar/Sidebar';
 import Content from './layout/Content/Content';
-
+import { useEffect } from 'react';
+import toast from 'react-hot-toast';
 
 function AdminApp() {
- 
+  useEffect(()=>{
+    toast.success("Admin panel will be Updating Soon!! Please Wait for Few Days")
+  },[])
   return (
     <>
       <div className='app'>
-        
         <Sidebar />
         <Content />
       </div>
