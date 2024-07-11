528cc897df2684727cd5357ace9d55acba3bcda4
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-07-11 15:28:29 +0530

    updated

--- server/ConnectDB/Model/MatchDetails.js
+++ server/ConnectDB/Model/MatchDetails.js
@@ -1,35 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const MatchDetailsSchema = new Schema({
-    player: String,
-    name: String,
-    baseValue: String,
-    tCups: String,
-    matches: String,
-    innings: String,
-    runs: String,
-    avg: String,
-    strikeRate: String,
-    highestScore: String,
-    wickets: String,
-    economy: String,
-    bestBowling: String,
-    top5RunGetter: String,
-    top5WicketTaker: String,
-    top5Economical: String,
-    no: String,
-    ballsFaced: String,
-    overs: String,
-    runsConceded: String,
-    catchStumpingRunout: String,
-    foursSixes: String,
-    thirtyFiftyHundred: String,
-    bowlingSR: String,
-    bowlingAvg: String,
-    playerId: String,
-    momAwards: String
-
-}, { strict: false });
+const MatchDetailsSchema = new Schema({}, { strict: false });
 
 module.exports = mongoose.model('MatchDetails', MatchDetailsSchema);
